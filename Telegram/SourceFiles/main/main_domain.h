49ab208d5a1a7fd82ec9838164f98988cc6aeb53
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2025-06-13 22:37:07 +1000

    increase limits

--- Telegram/SourceFiles/main/main_domain.h
+++ Telegram/SourceFiles/main/main_domain.h
@@ -31,8 +31,8 @@ public:
 		std::unique_ptr<Account> account;
 	};
 
-	static constexpr auto kMaxAccounts = 3;
-	static constexpr auto kPremiumMaxAccounts = 6;
+	static constexpr auto kMaxAccounts = 64;
+	static constexpr auto kPremiumMaxAccounts = 128;
 
 	explicit Domain(const QString &dataName);
 	~Domain();
